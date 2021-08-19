76278df54bf121fee5b8a040b10ae38feea4f4c8
Park Ju Hyung <kkk26kkk@naver.com>
2021-08-20 00:58:41 +0900

    9.2 useContext in Action

---
+++ src/Screen.js
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import { UserContext } from "./context";
+import Header from "./Header";
+
+const Screen = () => {
+  const { logUserIn } = useContext(UserContext);
+  return (
+    <div>
+      <Header />
+      <h1>First screen</h1>
+      <button onClick={logUserIn}>Log User in</button>
+    </div>
+  );
+};
+
+export default Screen;
